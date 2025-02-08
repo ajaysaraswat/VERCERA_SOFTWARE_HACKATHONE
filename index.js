acf3b5f21a81edb6aa0b155faf39c374ce6abfad
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-02-08 16:37:59 +0530

    designing of view pages

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const { connecttoMongoDB } = require("./connection/connection");
 const app = express();
+const userRouter = require("./routes/user");
 const youtubeRouter = require("./routes/youtube");
 const path = require("path");
 const PORT = 8000;
 connecttoMongoDB(process.env.MONGO_URL);
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
@@ -14,6 +16,8 @@ app.get("/", (req, res) => {
   return res.render("home");
 });
 app.use("/", youtubeRouter);
+app.use("/", userRouter);
+
 app.listen(PORT, (req, res) => {
   console.log(`server is running on port ${PORT}`);
 });
