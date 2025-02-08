34bc126abed37c8ffb437472104bb8c11e7c553c
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-02-08 14:49:37 +0530

    making home page for app

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require("express");
 require("dotenv").config();
 const { connecttoMongoDB } = require("./connection/connection");
 const app = express();
-const youtubeRouter = require("./routes/youtube")
+const youtubeRouter = require("./routes/youtube");
+const path = require("path");
 const PORT = 8000;
 connecttoMongoDB(process.env.MONGO_URL);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.get("/", (req, res) => {
   return res.json({ message: "working" });
 });
-app.use("/",youtubeRouter)
+app.use("/", youtubeRouter);
 app.listen(PORT, (req, res) => {
   console.log(`server is running on port ${PORT}`);
 });
-
